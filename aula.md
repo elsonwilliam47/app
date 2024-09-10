b18ec75afd8b8de3cc2d2e45a821e1b46a84f37d
Elson William <elsonwilliam2010@gmail.com>
2024-09-10 13:46:22 -0300

    menu da aplicação

--- aula.md
+++ aula.md
@@ -43,3 +43,12 @@ Operadores de atribuição de valor
 # Condicionais
 
 [x] Switch
+
+## Módulos em Node.js:
+
+- [x]Importação de módulos (require, CommonJS)
+- [x]Biblioteca 'inquirer' para criar prompts interativos
+
+# Programação assíncrona e Promises:
+
+- [x] Uso de funções assíncronas (async/await)
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function start(){
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
     while(true){
-        let opcao = "sair"
+
+        const opcao = await select({
+            message: "Menu>",
+            choices: [
+                {
+
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+
         switch(opcao){
             case "cadastrar":
                 console.log("vamos cadastrar")
@@ -13,3 +36,5 @@ function start(){
         }
     }
 }
+
+start()
